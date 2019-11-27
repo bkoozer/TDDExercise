210e82b55dba9c075a6f129b52f7a7d9570bc381
bkoozer <bill.koozer@limolink.com>
2019-11-26 18:08:37 -0600

    Applies .gitignore

---
+++ StringCalculator.Test/AddTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace StringCalculator.Test
+{
+    [TestFixture]
+    public class AddTests
+    {
+        [Test]
+        public void ShouldAddTwoNumbers()
+        {
+            Calculator calc = new Calculator();
+            Assert.AreEqual(3, calc.Add("1,2"));
+        }
+
+        [Test]
+        public void ShouldReturnZeroIfInputEmpty()
+        {
+            Calculator calc = new Calculator();
+            Assert.AreEqual(0, calc.Add(""));
+        }
+
+        [Test]
+        public void ShouldReturnZeroIfInputNull()
+        {
+            Calculator calc = new Calculator();
+            Assert.AreEqual(0, calc.Add(null));
+        }
+
+        [Test]
+        public void ShouldHandleUnknownAmountOfNumbers()
+        {
+            Calculator calc = new Calculator();
+            Assert.AreEqual(6, calc.Add("1,2,3"));
+            Assert.AreEqual(15, calc.Add("5,4,3,2,1"));
+        }
+        [Test]
+        public void ShouldAllowNewLineAsDelimiter()
+        {
+            Calculator calc = new Calculator();
+            Assert.AreEqual(6, calc.Add("1\n2,3"));
+        }
+        [Test]
+        public void ShouldAcceptAlternateDelimiters()
+        {
+            Calculator calc = new Calculator();
+            Assert.AreEqual(3, calc.Add("//:\n1:2"));
+            Assert.AreEqual(10, calc.Add("//*\n2*2,6"));
+        }
+    }
+}
---
+++ StringCalculator/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StringCalculator
+{
+    public class Calculator
+    {
+        List<char> delimiters = new List<char> {',','\n'};
+        public Calculator()
+        {
+        }
+
+        public int Add(string input)
+        {
+            if (String.IsNullOrEmpty(input))
+                return 0;
+            if (input.StartsWith("//"))
+            {
+                var newdelimiter = input.Split('\n')[0].Replace("//", "").ToCharArray();
+                delimiters.Add(newdelimiter[0]);
+                input = input.Replace("//" + newdelimiter[0] + "\n", "");
+            }
+            var nums = input.Split(delimiters.ToArray());
+            int total = 0;
+            foreach (var num in nums)
+            {
+                total += int.Parse(num);
+            }
+            return total;
+        }
+    }
+}
