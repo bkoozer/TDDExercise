8a2d8a768cc7ca3a6c9245cb108e1bdc0c9ebf60
bkoozer <bill.koozer@limolink.com>
2019-11-27 09:16:57 -0600

    Reduces verbosity  - uses Lambda instead of for loop for sum - and moves null or empty check to ProcessInput.

--- StringCalculator/Calculator.cs
+++ StringCalculator/Calculator.cs
@@ -1,30 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Linq;
 namespace StringCalculator
 {
     public class Calculator
     {
         List<char> delimiters = new List<char> {',','\n'};
         public Calculator()
-        {
-        }
+        { }
 
         public int Add(string input)
-        {
-            if (String.IsNullOrEmpty(input))
-                return 0;           
-            int total = 0;
-            var nums = ProcessInput(input);
-            foreach (var num in nums)
-            {
-                total += int.Parse(num);
-            }
+        {          
+            var total = ProcessInput(input).Sum(s=>int.Parse(s));
             return total;
         }
 
         public string[] ProcessInput(string input)
         {
+            if (string.IsNullOrEmpty(input))
+                return new string[] { "0" };
             if (input.StartsWith("//"))
             {
                 var newDelimiter = input.Split('\n')[0].Replace("//", "").ToCharArray()[0];
